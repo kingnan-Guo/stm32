392cd8965266bf8fc2d89028f39510d448ae76d3
kingnan <373734199@qq.com>
2024-03-26 00:09:18 +0800

    调整转速
    Angel 调整大小 调整转速
    当前 按钮功能 不生效
    若接的是 180 度舵机 那么会调整旋转角度

--- User/main.c
+++ User/main.c
@@ -5,8 +5,11 @@
 //#include "countSensor.h"
 #include "Timer.h"
 #include "servo.h"
+#include "key.h"
 
 uint8_t i;
+uint8_t keyNum;
+float Angel;
 
 int main(void) {
 //    OLED_Init();
@@ -16,28 +19,26 @@ int main(void) {
 //    OLED_ShowString(1, 4, "miaomiao");
 //    OLED_ShowNum(2, 1, birth,8);
 //    OLED_ShowSignedNum(3, 1, -88, 2);
-//    OLED_ShowHexNum(3, 5, 0xAABB, 4);
+//    OLED_ShowHexNum(3, 5, 0xAABB, 4);     ·`
 //    OLED_ShowBinNum(4, 1, 0xABAB, 16); // 十六 进制 转 二进制
 //    Delay_ms(2000);
 //    OLED_Clear();
 //    LED_Init();
-
+    Key_Init();
     OLED_Init();
     servo_init();
-    servo_setAngle(90);
+
     OLED_ShowString(1, 4, "miaomiao");
-
+//    Angel += 173;
 	while(1) {
-        keyNUm = key_GetName();
-        if(kekeyNUm == 1){
-            Angel += 30;
-            if (Angel >180){
-
-                Ange = 1000;
-
-            }
-        }
-        servo_setAngle(90);
-        OLED_ShowNum(1, 7, weke,3);
+        keyNum = Key_GetNum();
+//        if(keyNum == 1){
+//            Angel += 30;
+//            if (Angel >180){
+//                Angel = 1000;
+//            }
+//        }
+        servo_setAngle(Angel);
+        OLED_ShowNum(2, 1, Angel,3);
     };
 };
--- cmake-build-debug/CMakeFiles/stm32F103C8T6Project.elf.dir/User/main.c.obj.d
+++ cmake-build-debug/CMakeFiles/stm32F103C8T6Project.elf.dir/User/main.c.obj.d
@@ -38,4 +38,5 @@ CMakeFiles/stm32F103C8T6Project.elf.dir/User/main.c.obj: \
  /Users/kingnan/Documents/github/stm32/components/delay/delay.h \
  /Users/kingnan/Documents/github/stm32/components/oled/OLED.h \
  /Users/kingnan/Documents/github/stm32/components/Timer/Timer.h \
- /Users/kingnan/Documents/github/stm32/components/PWM/PWM.h
+ /Users/kingnan/Documents/github/stm32/components/servo/servo.h \
+ /Users/kingnan/Documents/github/stm32/components/key/key.h
