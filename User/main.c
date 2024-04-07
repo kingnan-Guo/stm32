41590fc427d0e630f9e62b1b3fa934185276ee1a
kingnan <373734199@qq.com>
2024-04-07 23:50:33 +0800

    清除无用代码

--- User/main.c
+++ User/main.c
@@ -50,22 +50,3 @@ int main(void) {
     }
 }
 
-
-/* 告知连接器不从C库链接使用半主机的函数 */
-#pragma import(__use_no_semihosting)
-
-/* 定义 _sys_exit() 以避免使用半主机模式 */
-void _sys_exit(int x)
-{
-    x = x;
-}
-
-/* 标准库需要的支持类型 */
-struct __FILE
-{
-    int handle;
-};
-
-FILE __stdout;
-
-
