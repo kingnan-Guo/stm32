2b05b3d0192fc2b6bf1d8d11da56b515bcae63b0
kingnan <373734199@qq.com>
2024-04-14 01:34:47 +0800

    更新打包

--- cmake-build-debug/CMakeFiles/stm32F103C8T6Project.elf.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/stm32F103C8T6Project.elf.dir/cmake_clean.cmake
@@ -83,8 +83,6 @@ file(REMOVE_RECURSE
   "CMakeFiles/stm32F103C8T6Project.elf.dir/components/oled/OLED.c.obj.d"
   "CMakeFiles/stm32F103C8T6Project.elf.dir/components/servo/servo.c.obj"
   "CMakeFiles/stm32F103C8T6Project.elf.dir/components/servo/servo.c.obj.d"
-  "CMakeFiles/stm32F103C8T6Project.elf.dir/components/unMicroLibRedirect/unMicroLibRedirect.c.obj"
-  "CMakeFiles/stm32F103C8T6Project.elf.dir/components/unMicroLibRedirect/unMicroLibRedirect.c.obj.d"
   "CMakeFiles/stm32F103C8T6Project.elf.dir/startup/startup_stm32f103c8tx.s.obj"
   "CMakeFiles/stm32F103C8T6Project.elf.dir/system/retarget/retarget.c.obj"
   "CMakeFiles/stm32F103C8T6Project.elf.dir/system/retarget/retarget.c.obj.d"
