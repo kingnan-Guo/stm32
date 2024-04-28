36ccedd80a84e4da886de4b6148f774390ab0c1e
kingnan <373734199@qq.com>
2024-04-28 23:37:34 +0800

    FreeRTOSConfig.h
    FreeRTOSConfig.h 配置

--- FreeRTOS/config/FreeRTOSConfig.h
+++ FreeRTOS/config/FreeRTOSConfig.h
@@ -46,24 +46,30 @@
 /* Section where include file can be added */
 /* USER CODE END Includes */
 
+//针对不同的编译器调用不同的stdint.h文件
 /* Ensure definitions are only used by the compiler, and not by the assembler. */
 #if defined(__ICCARM__) || defined(__CC_ARM) || defined(__GNUC__)
 #include <stdint.h>
-extern uint32_t SystemCoreClock;
+extern uint32_t SystemCoreClock;// 只是声明一下系统时钟 ，后面要用到系统时钟
 #endif
+
+//在这里 添加此句
+//断言 目的是 当 x = 0 时 调用这个函数 输出错误
+//#define vAssertCalled(char,int) printf("Error:%s,%d\r\n",char,int);
+//#define configASSERT(x) if((x)==0) vAssertCalled(__FILE__,__LINE__);
 
 /** ==========================================================================================================
  * 基础配置项
  * ==========================================================================================================
  */
-#define configUSE_PREEMPTION                     1
+#define configUSE_PREEMPTION                     1                          // 1 使用抢占式内核， 高优先级可以抢占低优先级 0 使用协程 （不再更新）
 #define configSUPPORT_STATIC_ALLOCATION          1
 #define configSUPPORT_DYNAMIC_ALLOCATION         1
 #define configUSE_IDLE_HOOK                      0
 #define configUSE_TICK_HOOK                      0
-#define configCPU_CLOCK_HZ                       ( SystemCoreClock )
-#define configTICK_RATE_HZ                       ((TickType_t)1000)
-#define configMAX_PRIORITIES                     ( 56 )
+#define configCPU_CLOCK_HZ                       ( SystemCoreClock )        // CPU频率 ； 告诉 FreeRTOS 当前MCU 的主频时多少； 在库函数中 Libraries/CMSIS/system_stm32f10x.c 下定义的 72MHZ ； 配置系统时钟的时候可被更改
+#define configTICK_RATE_HZ                       ((TickType_t)1000)         // FreeRTOS 这个系统的节拍频率；  时钟节拍频率，这里设置为1000， 一秒跳动1000次，周期就是1ms； 在 FreeRTOS/portable/GCC/ARM_CM3/portmacro.h 文件中 定义 TickType_t ； uint16_t
+#define configMAX_PRIORITIES                     ( 32 )                     // 可使用的最大优先级 ； 数字越大 执行的优先级 越高 ；理论上来说支持的优先级时不限制的 ；之前设置 56 ；只有当 configUSE_PORT_OPTIMISED_TASK_SELECTION 为 0 时 可以设置 32 以上的数；硬件指令最大支持 32 位；
 #define configMINIMAL_STACK_SIZE                 ((uint16_t)128)
 #define configTOTAL_HEAP_SIZE                    ((size_t)3072)
 #define configMAX_TASK_NAME_LEN                  ( 16 )
@@ -73,7 +79,7 @@ extern uint32_t SystemCoreClock;
 #define configQUEUE_REGISTRY_SIZE                8
 #define configUSE_RECURSIVE_MUTEXES              1
 #define configUSE_COUNTING_SEMAPHORES            1
-#define configUSE_PORT_OPTIMISED_TASK_SELECTION  0
+#define configUSE_PORT_OPTIMISED_TASK_SELECTION  1                              //1 启用特殊方法来选择下一个要运行的任务； 一般是硬件计算前导零指令，如果所使用的MCU没有这些硬件指令的话此宏应该设置为 0 使用软件区计算，但是有个前提是，当 configUSE_PORT_OPTIMISED_TASK_SELECTION  置 1 时； configMAX_PRIORITIES 只能小于 32； STM32 支持 硬件计算 所以可以配置成 1；
 
 /* Co-routine definitions. */
 #define configUSE_CO_ROUTINES                    0
@@ -85,13 +91,22 @@ extern uint32_t SystemCoreClock;
 #define configTIMER_QUEUE_LENGTH                 10
 #define configTIMER_TASK_STACK_DEPTH             256
 
+/**
+ *  在 课程中新加的一些 配置
+ *
+ */
+#define configUSE_TIME_SLICING					 1						    // 1 使能时间片调度(默认式使能的) （同优先级的 任务 使用这个 时间片 调度）
+#define configUSE_TICKLESS_IDLE					 0                          // 1 启用 低功耗 tickless 模式  ； 自带的 tickless 模式
+#define configUSE_QUEUE_SETS					 1                          // 1 为1时启用队列集
+
+
 /** ==========================================================================================================
  * 可选函数 配置选项
  * ==========================================================================================================
  */
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
-#define INCLUDE_vTaskPrioritySet            1
+#define INCLUDE_vTaskPrioritySet            1       // 设置优先级
 #define INCLUDE_uxTaskPriorityGet           1
 #define INCLUDE_vTaskDelete                 1
 #define INCLUDE_vTaskCleanUpResources       0
