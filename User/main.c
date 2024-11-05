8dfef6a6053fee0c04186337d9a7c38ca4230d16
kingnan-Guo <373734199@qq.com>
2024-11-05 22:43:46 +0800

    Update main.c

--- User/main.c
+++ User/main.c
@@ -6,9 +6,20 @@
 int main(void) {
     // 初始化 I2C 的引脚
     OLED_Init();
-    OLED_ShowString(1, 1, "cubeMX_original");
+//    OLED_ShowString(1, 1, "cubeMX_original");
 
-    xTaskCreateFun2Main();
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+
+    GPIO_InitTypeDef GPIO_InitStruct;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_13;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    GPIO_ResetBits(GPIOA, GPIO_Pin_13);
+
+
+//    xTaskCreateFun2Main();
     while(1) {
 
     }
