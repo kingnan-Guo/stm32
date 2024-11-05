d74547e7882bf707e93a99eba78f4092aacf5bc1
kingnan <373734199@qq.com>
2024-11-05 22:36:28 +0800

    更新注释

--- components/LED/LED.c
+++ components/LED/LED.c
@@ -5,7 +5,7 @@ void LED_Init(void){
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
 
     GPIO_InitTypeDef GPIO_InitStructure;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;// 推挽
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15 | GPIO_Pin_14 | GPIO_Pin_13;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 
