f97eeb8c3b7cb9092897ed65a0165728fa5f25d4
kingnan <373734199@qq.com>
2024-11-12 23:01:26 +0800

    PWM电灯

--- User/main.c
+++ User/main.c
@@ -2,16 +2,16 @@
 #include "OLED.h"
 #include "delay.h"
 #include <stdio.h>
-#include "EXTI_Interrupt.h"
+#include "PWM_R.h"
 
-// RCC_APB2Periph_GPIOA pin14 接口  下降沿触发
 int main(void) {
     // 初始化 I2C 的引脚
     OLED_Init();
-    EXTI_Interrup_R_Init();
-    while(1) {
-        // OLED_ShowNum(1,1,getTIM2Count(), 5);
-        // OLED_ShowNum(2,1,getExtiInterruptCount(), 5);
-    }
+    OLED_ShowNum(1,1,2, 5);
+    PWM_R_Init();
+//    while(1) {
+//        // OLED_ShowNum(1,1,getTIM2Count(), 5);
+//        // OLED_ShowNum(2,1,getExtiInterruptCount(), 5);
+//    }
 }
 
--- restarComponets/PWM_R/PWM_R.c
+++ restarComponets/PWM_R/PWM_R.c
@@ -7,6 +7,7 @@
 
 
 void PWM_R_Init(void){
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 
     // 初始化  引脚 推挽输出
@@ -19,7 +20,7 @@ void PWM_R_Init(void){
 
     // 开启 计数器
 
-    RCC_APB1PeriphClockCmd(RCC_APB1ENR_TIM2EN, ENABLE);
+
     TIM_InternalClockConfig(TIM2);
 
 
@@ -27,8 +28,8 @@ void PWM_R_Init(void){
     // 由于要对 信号 进行采样 判定， 所以 使用 内部时钟的频率 作为采样的 频率， 而 TIM_ClockDivision 就是 将采样频率分频 划分，采样次数越多 精度越高，但是 延时 增加
     TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;// 指定时钟划分频  TIM_CKD_DIV1 一分频，
     TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;// 计数 模式， 向上计数
-    TIM_TimeBaseInitStruct.TIM_Period = 100 -1 ;// 计数器 重装 器的值； 要在  0~ 65535 以内
-    TIM_TimeBaseInitStruct.TIM_Prescaler = 720 - 1;// 预分频器 的值; 这里目的 是 定时 一秒钟；  要在 0~ 65535 以内
+    TIM_TimeBaseInitStruct.TIM_Period = 100 -1 ;// ARR 计数器 重装 器的值； 要在  0~ 65535 以内
+    TIM_TimeBaseInitStruct.TIM_Prescaler = 720 - 1;// PSC 预分频器 的值; 这里目的 是 定时 一秒钟；  要在 0~ 65535 以内
     TIM_TimeBaseInitStruct.TIM_RepetitionCounter = 0;// 指定重复计数器值； 高级计数器 才有的 给 0
     //配置时机 单元
     TIM_TimeBaseInit(TIM2, &TIM_TimeBaseInitStruct);
@@ -40,9 +41,9 @@ void PWM_R_Init(void){
     TIM_OCInitTypeDef TIM_OCInitStructure;
     TIM_OCStructInit(&TIM_OCInitStructure);// 初始化默认参数
     TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;// 选择 PWM1 模式
-    TIM_OCInitStructure.TIM_OCNPolarity = ;// 输出比较极性
+    TIM_OCInitStructure.TIM_OCNPolarity = TIM_OCPolarity_High;// 输出比较极性 ; OC1 ref
     TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;// 输出使能 开启
-    TIM_OCInitStructure.TIM_Pulse = 0;//
+    TIM_OCInitStructure.TIM_Pulse = 10;// CCR
     TIM_OC1Init(TIM2, &TIM_OCInitStructure);
 
     //启动定时器
