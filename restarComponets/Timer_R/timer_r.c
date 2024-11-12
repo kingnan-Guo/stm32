8ac93782e46d8622370a9e75b321dfa38afb9635
kingnan-Guo <373734199@qq.com>
2024-11-12 14:50:49 +0800

    注掉 中断 函数

--- restarComponets/Timer_R/timer_r.c
+++ restarComponets/Timer_R/timer_r.c
@@ -52,14 +52,15 @@ uint16_t getTime2InterruptCount(){
     return time2InterruptCount;
 }
 
-void TIM2_IRQHandler(void){
-    /// TIM_IT_Update 代表 要看 哪个中断标志位
-    if(TIM_GetITStatus(TIM2, TIM_IT_Update) == SET){
-        time2InterruptCount++;
-        // 清除标志位
-        TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
-    }
-}
+// 跟  restarComponets/EXTI_Interrupt/EXTI_Interrupt.c 和 components/Timer/Timer.c 的 中断函数 重复了 所以 ，这里注掉
+//void TIM2_IRQHandler(void){
+//    /// TIM_IT_Update 代表 要看 哪个中断标志位
+//    if(TIM_GetITStatus(TIM2, TIM_IT_Update) == SET){
+//        time2InterruptCount++;
+//        // 清除标志位
+//        TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+//    }
+//}
 
 
 //
