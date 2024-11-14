01c255f7a2ff1cf6942439c32d3c76616ec34231
kingnan-Guo <373734199@qq.com>
2024-11-14 11:29:00 +0800

    编码器测速

--- User/main.c
+++ User/main.c
@@ -7,16 +7,7 @@
 
 int main(void) {
     OLED_Init();
-    PWM_R_Init();
-    INPUTCAPTURE_R_INIT();
-    PWM_R_SetPrescaler(720 -1); // 频率 Freq = 72M / (PSC + 1) / 100
-    PWM_R_SetCompare1(42); // 占空比 Duty = 50;
     OLED_ShowNum(1,1, 0, 5);
-    OLED_ShowNum(1,1, TIM_GetCapture1(TIM2), 5);
-    OLED_ShowNum(2,1, TIM_GetPrescaler(TIM2), 5);
     while(1) {
-
-        OLED_ShowNum(3,1,getICFreq(), 10);
-        OLED_ShowNum(4,1,getICDuty(), 10);
     }
 }
---
+++ restarComponets/ENCODE_R/ENCODE_R.c
@@ -0,0 +1,5 @@
+//
+// Created by 90175 on 2024/11/14.
+//
+
+#include "ENCODE_R.h"
