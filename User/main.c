2ee5297255008049911c27b54c4cd698b4c0351a
kingnan <373734199@qq.com>
2024-11-14 23:20:19 +0800

    Update main.c

--- User/main.c
+++ User/main.c
@@ -2,15 +2,11 @@
 #include "OLED.h"
 #include "delay.h"
 #include <stdio.h>
-#include "ADC_MULTI_CHANNEL.h"
 
-uint16_t ADValue;
-float Voltage;
+
 int main(void) {
     OLED_Init();
-    ADC_MULTI_CHANNEL_INIT();
     while(1) {
-        OLED_ShowNum(1, 1, ADC_MULTI_CHANNEL_GetValue(ADC_Channel_0), 4);
-        OLED_ShowNum(2, 1, ADC_MULTI_CHANNEL_GetValue(ADC_Channel_1), 4);
+        OLED_ShowNum(1, 1, 0, 9);
     }
 }
