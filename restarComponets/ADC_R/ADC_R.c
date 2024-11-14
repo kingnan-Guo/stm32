e5bb35f770d5844d19725a725c5257d78b95590f
kingnan <373734199@qq.com>
2024-11-14 22:22:19 +0800

    Update ADC_R.c

--- restarComponets/ADC_R/ADC_R.c
+++ restarComponets/ADC_R/ADC_R.c
@@ -28,7 +28,7 @@ void ADC_R_INIT(){
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
     GPIO_InitTypeDef GPIO_InitStructure;
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;//  模拟输入
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;//  模拟输入
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
 
