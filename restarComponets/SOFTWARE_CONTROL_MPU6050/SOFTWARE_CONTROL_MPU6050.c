a6ae44b0cc47b5a94576c81c6a06fb229ad7c734
kingnan-Guo <373734199@qq.com>
2024-11-18 20:13:13 +0800

    创建文件

---
+++ restarComponets/SOFTWARE_CONTROL_MPU6050/SOFTWARE_CONTROL_MPU6050.c
@@ -0,0 +1,10 @@
+//
+// Created by 90175 on 2024/11/18.
+//
+#include "stm32f10x.h"
+#include "SOFTWARE_CONTROL_MPU6050.h"
+
+
+void SOFTWARE_CONTROL_MPU6050(){
+
+}
---
+++ restarComponets/SOFTWARE_I2C/SOFTWARE_I2C.c
@@ -0,0 +1,5 @@
+//
+// Created by 90175 on 2024/11/18.
+//
+
+#include "SOFTWARE_I2C.h"
