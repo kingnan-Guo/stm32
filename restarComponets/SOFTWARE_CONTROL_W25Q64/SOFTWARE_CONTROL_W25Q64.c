2a8bec96718b15ec99e52376511718a2c00924d0
kingnan-Guo <373734199@qq.com>
2024-11-20 17:11:28 +0800

    软件 读取 W25Q64

--- restarComponets/SOFTWARE_CONTROL_W25Q64/SOFTWARE_CONTROL_W25Q64.c
+++ restarComponets/SOFTWARE_CONTROL_W25Q64/SOFTWARE_CONTROL_W25Q64.c
@@ -1,5 +1,175 @@
 //
 // Created by 90175 on 2024/11/20.
 //
-
+#include "stm32f10x.h"
+#include "SOFTWARE_SPI.h"
 #include "SOFTWARE_CONTROL_W25Q64.h"
+#include "W25Q64_Ins.h"
+#include "OLED.h"
+
+
+void SOFTWARE_CONTROL_W25Q64_INIT(){
+    SOFTWARE_SPI_INIT();
+}
+
+// 写使能; 每一次 页编程 都需要进行一次 开启写使能
+void SOFTWARE_CONTROL_W25Q64_WRITE_ENABLE(){
+    SOFTWARE_SPI_START();
+    SOFTWARE_SPI_SWAPBYTE(W25Q64_WRITE_ENABLE);
+    SOFTWARE_SPI_STOP();
+}
+
+// 查看 BUSY 标志位
+void SOFTWARE_CONTROL_W25Q64_WAIT_BUSY(){
+    uint32_t TimeOut;
+    SOFTWARE_SPI_START();
+
+
+    SOFTWARE_SPI_SWAPBYTE(W25Q64_READ_STATUS_REGISTER_1); // 读状态寄存器1  ; 发送指令后 开始读取； BUSY 在最低位
+    TimeOut = 10000;
+    // 读取 Busy 位； 使用 FF 交换
+    while ((SOFTWARE_SPI_SWAPBYTE(W25Q64_DUMMY_BYTE) & 0x01) == 0x01){
+        TimeOut--;
+        if(TimeOut == 0){
+            break;
+        }
+    };
+    SOFTWARE_SPI_STOP();
+}
+
+
+// 去读取数据， 用FF 交换
+void SOFTWARE_CONTROL_W25Q64_READ_DATA(uint32_t Address, uint8_t * DATA_ARRAY,uint32_t Count){
+    uint32_t i;
+    SOFTWARE_SPI_START();
+
+    SOFTWARE_SPI_SWAPBYTE(W25Q64_READ_DATA);// 读指令
+
+    // 读地址
+    SOFTWARE_SPI_SWAPBYTE(Address >> 16);// 最高的 八位
+    SOFTWARE_SPI_SWAPBYTE(Address >> 8);// 中间 八位
+    SOFTWARE_SPI_SWAPBYTE(Address);// 低 八位
+    for ( i = 0; i < Count; i++) {
+        // 使用 0xFF 去交换数据
+        DATA_ARRAY[i] = SOFTWARE_SPI_SWAPBYTE(W25Q64_DUMMY_BYTE);
+    }
+
+
+    SOFTWARE_SPI_STOP();
+
+
+}
+
+// 页编程
+// 这里 要传 指针，也就是  ARRAY 数据的 第一个数据的 指针地址
+void SOFTWARE_CONTROL_W25Q64_PAGE_PROGRAM(uint32_t Address, uint8_t * DATA_ARRAY,uint32_t Count){
+    SOFTWARE_CONTROL_W25Q64_WRITE_ENABLE();// 写使能
+    SOFTWARE_SPI_START();
+
+    SOFTWARE_SPI_SWAPBYTE(W25Q64_PAGE_PROGRAM);// 写 指令
+
+
+    SOFTWARE_SPI_SWAPBYTE(Address >> 16);// 最高的 八位
+    SOFTWARE_SPI_SWAPBYTE(Address >> 8);// 中间 八位
+    SOFTWARE_SPI_SWAPBYTE(Address);// 低 八位
+
+    for (int i = 0; i < Count; ++i) {
+        SOFTWARE_SPI_SWAPBYTE(DATA_ARRAY[i]);
+    }
+
+
+    SOFTWARE_SPI_STOP();
+    SOFTWARE_CONTROL_W25Q64_WAIT_BUSY();// 等待 BUSY
+}
+
+// 擦除的功能
+// 扇区  擦除，因为扇区  是4K
+// 一次性接受 32 位 地址后，分三次发送给 W25Q64
+void SOFTWARE_CONTROL_W25Q64_SECTOR_ERASE(uint32_t Address){
+    SOFTWARE_CONTROL_W25Q64_WRITE_ENABLE();// 写使能
+    SOFTWARE_SPI_START();
+
+    SOFTWARE_SPI_SWAPBYTE(W25Q64_SECTOR_ERASE_4KB);
+
+    SOFTWARE_SPI_SWAPBYTE(Address >> 16);// 最高的 八位
+    SOFTWARE_SPI_SWAPBYTE(Address >> 8);// 中间 八位
+    SOFTWARE_SPI_SWAPBYTE(Address);// 低 八位
+
+
+    SOFTWARE_SPI_STOP();
+
+    SOFTWARE_CONTROL_W25Q64_WAIT_BUSY();// 等待 BUSY 大概 30ms
+}
+
+
+// 读取 ID
+void SOFTWARE_CONTROL_W25Q64_READ_ID(uint8_t *MID, uint16_t *DID){
+    SOFTWARE_CONTROL_W25Q64_WRITE_ENABLE();// 写使能
+    SOFTWARE_SPI_START();
+
+    // 发送指令
+    SOFTWARE_SPI_SWAPBYTE(W25Q64_JEDEC_ID);
+    // 读取
+    // W25Q64 收到指令后 ，想主机发送 字节，之际 通过 W25Q64_DUMMY_BYTE 0xFF 将数据交换过来； 0xFF 没有任何意义
+    *MID = SOFTWARE_SPI_SWAPBYTE(W25Q64_DUMMY_BYTE);
+    // 接收 以时序传过来的数据
+    // 在交换一次 就时设备 id 的高 8 位
+    *DID = SOFTWARE_SPI_SWAPBYTE(W25Q64_DUMMY_BYTE);
+    // *DID 左移 8位
+    *DID <<= 8;
+    // 再交换一次 就时设备 id 的低 8 位
+    *DID = *DID | SOFTWARE_SPI_SWAPBYTE(W25Q64_DUMMY_BYTE);
+
+
+    SOFTWARE_SPI_STOP();
+    SOFTWARE_CONTROL_W25Q64_WAIT_BUSY();// 等待 BUSY
+}
+
+
+
+
+void SOFTWARE_CONTROL_W25Q64_MAIN(){
+
+    uint8_t MID;
+    uint16_t DID;
+    uint8_t dataArray_write[] = {0x01, 0x03, 0x05,0x07};
+    uint8_t dataArr_Read[4];
+
+    uint32_t startAddr = 0x000000;
+
+    SOFTWARE_CONTROL_W25Q64_INIT();
+
+    // 读 ID =======
+    SOFTWARE_CONTROL_W25Q64_READ_ID(&MID, &DID);
+    OLED_ShowString(1, 1, "MID ");
+    OLED_ShowHexNum(1, 5, MID, 2);
+    OLED_ShowString(2, 1, "DID ");
+    OLED_ShowHexNum(2, 5, DID, 4);
+
+
+
+
+    // 读写数据  =====
+
+    OLED_ShowString(3, 1, "W: ");
+    OLED_ShowString(4, 1, "R: ");
+
+    // 写入前先擦除
+    SOFTWARE_CONTROL_W25Q64_SECTOR_ERASE(startAddr);
+    // 写入数据
+    SOFTWARE_CONTROL_W25Q64_PAGE_PROGRAM(startAddr, dataArray_write, 4);
+    // 读取数据
+    SOFTWARE_CONTROL_W25Q64_READ_DATA(startAddr, dataArr_Read, 4);
+
+
+    OLED_ShowHexNum(3, 3, dataArray_write[0], 2);
+    OLED_ShowHexNum(3, 6, dataArray_write[1], 2);
+    OLED_ShowHexNum(3, 9, dataArray_write[2], 2);
+    OLED_ShowHexNum(3, 12, dataArray_write[3], 2);
+
+    OLED_ShowHexNum(4, 3, dataArr_Read[0], 2);
+    OLED_ShowHexNum(4, 6, dataArr_Read[1], 2);
+    OLED_ShowHexNum(4, 9, dataArr_Read[2], 2);
+    OLED_ShowHexNum(4, 12, dataArr_Read[3], 2);
+
+}
--- restarComponets/SOFTWARE_SPI/SOFTWARE_SPI.c
+++ restarComponets/SOFTWARE_SPI/SOFTWARE_SPI.c
@@ -1,5 +1,80 @@
 //
 // Created by 90175 on 2024/11/20.
 //
-
+#include "stm32f10x.h"
 #include "SOFTWARE_SPI.h"
+
+// 0 模式
+#define SPI_PORT    GPIOA
+#define SPI_SCK     GPIO_Pin_5  // SCK      SPI 时钟              是 硬件控制的 所以配置 复用推挽输出
+#define SPI_D0      GPIO_Pin_6  // MISO     SPI主机输入从机输出     硬件外设输入信号 上拉输入； 普通GPIO 可以输入，外设也可以输入
+#define SPI_D1      GPIO_Pin_7  // MOSI     SPI主机输出从机输入     是 硬件控制的 所以配置 复用推挽输出
+#define SPI_CS      GPIO_Pin_4  // SS       SPI 片选              软件控制的 控制信号 ，所以 通用推挽输出
+
+void SOFTWARE_SPI_W_CS(uint8_t BitValue){
+    GPIO_WriteBit(SPI_PORT, SPI_CS, (BitAction)BitValue);
+}
+
+void SOFTWARE_SPI_INIT(){
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+    // 初始化 GPIO
+    GPIO_InitTypeDef GPIO_InitStructure;
+    // 开漏输出 有输入能力
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_7;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    // 主机输入到从机 要有 一定的 驱动能力
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    SOFTWARE_SPI_W_CS(1);
+
+    SOFTWARE_SPI_W_CS(0);
+}
+
+
+
+
+void SOFTWARE_SPI_W_SCK(uint8_t BitValue){
+    GPIO_WriteBit(SPI_PORT, SPI_SCK, (BitAction)BitValue);
+}
+
+uint8_t SOFTWARE_SPI_R_MISO(){
+    return GPIO_ReadInputDataBit(SPI_PORT, SPI_D0);
+}
+
+void SOFTWARE_SPI_W_MOSI(uint8_t BitValue){
+    GPIO_WriteBit(SPI_PORT, SPI_D1, (BitAction)BitValue);
+}
+
+void SOFTWARE_SPI_START(){
+    SOFTWARE_SPI_W_CS(0);
+}
+
+void SOFTWARE_SPI_STOP(){
+    SOFTWARE_SPI_W_CS(1);
+}
+
+// 发送数据
+// CS 拉低
+// 先放置数据 然后
+uint8_t SOFTWARE_SPI_SWAPBYTE(uint8_t BYTE){
+    uint8_t i;
+    uint8_t RECEIVE_BYTE = 0x00;
+    for ( i = 0; i < 8; i++) {
+        SOFTWARE_SPI_W_MOSI(BYTE & (0x80 >> i));
+        SOFTWARE_SPI_W_SCK(1);// 上升沿发送数据
+        if(SOFTWARE_SPI_R_MISO() == 1){
+            RECEIVE_BYTE |= (0x80 >> i);
+        }
+        SOFTWARE_SPI_W_SCK(0);// 时钟 下降沿 采集
+    }
+    return RECEIVE_BYTE;
+}
+
+
