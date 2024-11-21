b2d05b34a8672f929d6b6bebde08539b418aa068
kingnan <373734199@qq.com>
2024-11-21 23:22:30 +0800

    回归 FreeRtos

--- User/main.c
+++ User/main.c
@@ -2,20 +2,11 @@
 #include "OLED.h"
 #include "delay.h"
 #include <stdio.h>
-#include "SOFTWARE_CONTROL_MPU6050.h"
-#include "HARDWARE_CONTROL_MPU6050.h"
-#include "HARDWARE_CONTROL_W25Q64.h"
+#include "FreeRTOSDemo.h"
 
 int main(void) {
     OLED_Init();
-
-
-
-//    SOFTWARE_CONTROLL_MPU6050_MAIN();
-
-//    HARDWARE_CONTROL_MPU6050_MAIN();
-
-HARDWARE_CONTROL_W25Q64_MAIN();
+    FreeRTOSDemoMain();
 
     while(1) {
     }
