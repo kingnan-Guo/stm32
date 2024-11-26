aeb624806910011978a5de709f826a8f7734847f
kingnan-Guo <373734199@qq.com>
2024-11-26 20:04:06 +0800

    更新

--- components/FreeRTOSTech/FREERTOS_LIST/FREERTOS_LIST.c
+++ components/FreeRTOSTech/FREERTOS_LIST/FREERTOS_LIST.c
@@ -6,11 +6,10 @@
 #include "OLED.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "list.h"
 #include "Serial.h"
 #include "retarget.h"
 #include "FREERTOS_LIST.h"
-
-
 
 //任务优先级
 #define START_TASK_LIST_PRIO		1
@@ -18,22 +17,21 @@
 #define START_STK_SIZE 		        128
 //任务句柄
 TaskHandle_t    START_TASK_HANDLER_LIST;
-
 TaskHandle_t    TASK1_HANDLER_LIST;
 TaskHandle_t    TASK2_HANDLER_LIST; // 存放任务句柄
 TaskHandle_t    DELETE_HANDLER_LIST;
+TaskHandle_t    TASK3_HANDLER_LIST;
 
+
+// 列表项
+List_t TEST_LIST;
+ListItem_t TEST_LIST_ITEM1;
+ListItem_t TEST_LIST_ITEM2;
+ListItem_t TEST_LIST_ITEM3;
 
 void vTASK1_LIST(void *pvParameters){
     int16_t num = 0x00;
     while (1) {
-        if(num == 5){
-            printf("printf delete task2\r\n");
-            vTaskSuspend(TASK2_HANDLER_LIST);// 挂起
-        }
-        if(num == 10){
-            vTaskResume(TASK2_HANDLER_LIST);// 重
-        }
         printf("printf task1=%d\r\n",  num);
         OLED_ShowString(1, 1, "task1");
         OLED_ShowNum(2, 1, num, 5);
@@ -42,9 +40,6 @@ void vTASK1_LIST(void *pvParameters){
     }
 }
 
-
-#define vTask2F_uxStackDepth    128
-#define vTask2F_uxPriority      5
 void vTASK2_LIST(void *pvParameters){
     int16_t num = 0x00;
     while (1) {
@@ -55,15 +50,34 @@ void vTASK2_LIST(void *pvParameters){
     }
 }
 
+void vTASK3_LIST(void *pvParameters){
+    vListInitialise(&TEST_LIST);// 初始化 列表
+    vListInitialiseItem(&TEST_LIST_ITEM1);// 初始化 列表项
 
+    TEST_LIST_ITEM1.xItemValue = 40;
+    TEST_LIST_ITEM2.xItemValue = 60;
+    TEST_LIST_ITEM3.xItemValue = 50;
+
+
+    printf("列表 和列表项地址  \r\n");
+    printf("项目                  地址                  \r\n");
+    printf("TEST_LIST                                  \r\n");
+    printf("TEST_LIST->pxIndex    %#x                  \r\n", (int)&TEST_LIST.pxIndex);
+    printf("TEST_LIST->xListEnd   %#x                  \r\n", (int)&TEST_LIST.xListEnd);
+    printf("TEST_LIST_ITEM1       %#x                  \r\n", (int)&TEST_LIST_ITEM1);
+    printf("TEST_LIST_ITEM2       %#x                  \r\n", (int)&TEST_LIST_ITEM2);
+    printf("TEST_LIST_ITEM3       %#x                  \r\n", (int)&TEST_LIST_ITEM3);
+
+
+    while (1) {
+
+    }
+}
 
 void vDELETE_LIST(void *pvParameters){
     while (1) {
     }
 }
-
-
-
 
 
 //开始任务任务函数
@@ -87,6 +101,18 @@ void START_TASK_LIST(void *pvParameters)
             (UBaseType_t)                                3,// 任务优先级 范围 0～ configMAX_PRIORITIES-1
             (TaskHandle_t *)                         &TASK2_HANDLER_LIST // 任务句柄，任务创建成功以后会返回次惹怒我的任务句柄， 这个 句柄其实就是任务的 任务堆栈，此参数 就用来保存这个任务句柄；其他API函数可能会使用到这个 句柄
     );
+
+    xTaskCreate(
+            (TaskFunction_t)                           vTASK3_LIST, // 任务函数
+            (char *  )                                    "vTask3",// 函数 名称， 任务名称长度不要超过  configMAX_TASK_NAME_LEN。
+            (const configSTACK_DEPTH_TYPE)            128,// 任务堆栈大小 ，注意 ： 实际申请到的堆栈是 uxStackDepth 的 4 倍（ 一个 StackType_t 是 32 位  4 个字节）， 其中空闲任务 的堆栈大小为    configMINIMAL_STACK_SIZE。
+            // (uint32_t)                                         vTask2F_uxStackDepth,
+            (void *  )                                "free2 0",// 传递给任务函数的参数
+            (UBaseType_t)                                3,// 任务优先级 范围 0～ configMAX_PRIORITIES-1
+            (TaskHandle_t *)                         &TASK3_HANDLER_LIST // 任务句柄，任务创建成功以后会返回次惹怒我的任务句柄， 这个 句柄其实就是任务的 任务堆栈，此参数 就用来保存这个任务句柄；其他API函数可能会使用到这个 句柄
+    );
+
+
 
     vTaskDelete(START_TASK_HANDLER_LIST); //删除开始任务;  为什么执行完成要删除？？？
     //taskEXIT_CRITICAL();            //退出临界区
