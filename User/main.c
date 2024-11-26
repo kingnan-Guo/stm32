389f7c84626a4b1fbbaea275618431758ceeac2b
kingnan <373734199@qq.com>
2024-11-26 23:18:03 +0800

    Update main.c

--- User/main.c
+++ User/main.c
@@ -2,17 +2,11 @@
 #include "OLED.h"
 #include "delay.h"
 #include <stdio.h>
-#include "xTaskSuspendResume.h"
-//#include "FREERTOS_LIST.h"
-#include "FreeRTOS_NVIC.h"
 #include "timer_r.h"
-#include "FREERTOS_LIST.h"
 
 
 int main(void) {
     OLED_Init();
-
-    FREERTOS_NVIC_MAIN();
     while(1) {
 
     }
