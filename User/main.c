db5644181ed83f14ed6226f4dff229199fd7c262
kingnan <373734199@qq.com>
2024-11-30 00:42:06 +0800

    Update main.c

--- User/main.c
+++ User/main.c
@@ -9,10 +9,10 @@
 
 int main(void) {
     OLED_Init();
-//    OLED_ShowString(1,1,"abc");
-//    FREERTOS_QUEUE_SET_MAIN();
-//FREERTOS_TEMP_MAIN();
-    FREERTOS_QUEUE_MAIN();
+    OLED_ShowString(1,1,"abc");
+////    FREERTOS_QUEUE_SET_MAIN();
+////FREERTOS_TEMP_MAIN();
+//    FREERTOS_QUEUE_MAIN();
     while(1) {
     }
 }
