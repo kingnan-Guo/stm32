4c326cb2fc4552810cbd944452176721f93d94a5
kingnan <373734199@qq.com>
2024-12-03 00:57:17 +0800

    任务组 同步点

--- User/main.c
+++ User/main.c
@@ -3,15 +3,13 @@
 #include "delay.h"
 #include <stdio.h>
 #include "timer_r.h"
-//#include "FREERTOS_QUEUE_SET.h"
-//#include "FREERTOS_TEMP.h"
-//#include "FREERTOS_QUEUE.h"
+
 //#include "FREERTOS_MUTEX.h"
 //#include "FREERTOS_SEMAPHORE.h"
 //#include "FREERTOS_MUTEX_PRIORITY_INVERSION.h"
 //#include "FREERTOS_RECURSIVE_MUTEX.h"
-#include "FREERTOS_EVENT_GROUP.h"
-//#include "FREERTOS_EVENT_GROUP_SYNC.h"
+//#include "FREERTOS_EVENT_GROUP.h"
+#include "FREERTOS_EVENT_GROUP_SYNC.h"
 
 
 int main(void) {
@@ -19,10 +17,10 @@ int main(void) {
 //    FREERTOS_MUTEX_MAIN();
 //    FREERTOS_SEMAPHORE_MAIN();
 //    FREERTOS_MUTEX_PRIORITY_INVERSION_MAIN();
-
 //    FREERTOS_RECURSIVE_MUTEX_MAIN();
+//    FREERTOS_EVENT_GROUP_MAIN();
 
-    FREERTOS_EVENT_GROUP_MAIN();
+    FREERTOS_EVENT_GROUP_SYNC_MAIN();
     while(1) {
 
 
--- components/FreeRTOSTech/FREERTOS_EVENT_GROUP_SYNC/FREERTOS_EVENT_GROUP_SYNC.c
+++ components/FreeRTOSTech/FREERTOS_EVENT_GROUP_SYNC/FREERTOS_EVENT_GROUP_SYNC.c
@@ -27,11 +27,11 @@
 //任务堆栈大小
 #define START_STK_SIZE 		                                 128
 
-#define vTASK1_EVENT_GROUP_SYNC_FUNCTION_uxStackDepth        512
+#define vTASK1_EVENT_GROUP_SYNC_FUNCTION_uxStackDepth        640
 #define vTASK1_EVENT_GROUP_SYNC_FUNCTION_uxPriority          1
-#define vTASK2_EVENT_GROUP_SYNC_FUNCTION_uxStackDepth        512
+#define vTASK2_EVENT_GROUP_SYNC_FUNCTION_uxStackDepth        640
 #define vTASK2_EVENT_GROUP_SYNC_FUNCTION_uxPriority          2
-#define vTASK3_EVENT_GROUP_SYNC_FUNCTION_uxStackDepth        512
+#define vTASK3_EVENT_GROUP_SYNC_FUNCTION_uxStackDepth        640
 #define vTASK3_EVENT_GROUP_SYNC_FUNCTION_uxPriority          3
 
 //任务句柄
@@ -55,14 +55,14 @@ static EventGroupHandle_t EVENT_GROUP_CALC;
 
 
 void vTASK1_EVENT_GROUP_SYNC(void *pvParameters){
-    const TickType_t xDelay100ms = pdMS_TO_TICKS( 100UL );
+    const TickType_t xDelay100ms = pdMS_TO_TICKS( 1000UL );
     int i = 0;
     while (1) {
         // 开始 做
-        printf("%s is do  one %d \r\n", (char * ) pvParameters, i);
+        printf("%s doing %d \r\n", (char * ) pvParameters, i);
         // 我已经做好 ，要等 所有人 都做好
         xEventGroupSync(EVENT_GROUP_CALC, ONE, ALL, portMAX_DELAY);
-        printf("%s is all Done  %d  \r\n", (char * ) pvParameters, i++);
+        printf("%s Done %d  \r\n", (char * ) pvParameters, i++);
         vTaskDelay(xDelay100ms);
     }
 }
@@ -70,14 +70,14 @@ void vTASK1_EVENT_GROUP_SYNC(void *pvParameters){
 
 
 void vTASK2_EVENT_GROUP_SYNC(void *pvParameters){
-    const TickType_t xDelay100ms = pdMS_TO_TICKS( 100UL );
+    const TickType_t xDelay100ms = pdMS_TO_TICKS( 1000UL );
     int i = 0;
     while (1) {
         // 开始 做
-        printf("%s is do  one %d \r\n", (char * ) pvParameters, i);
+        printf("%s doing %d \r\n", (char * ) pvParameters, i);
         // 我已经做好 ，要等 所有人 都做好
         xEventGroupSync(EVENT_GROUP_CALC, TWO, ALL, portMAX_DELAY);
-        printf("%s is all Done  %d  \r\n", (char * ) pvParameters, i++);
+        printf("%s Done %d  \r\n", (char * ) pvParameters, i++);
         vTaskDelay(xDelay100ms);
     }
 }
@@ -85,14 +85,14 @@ void vTASK2_EVENT_GROUP_SYNC(void *pvParameters){
 
 // 这时第一个 处理的任务
 void vTASK3_EVENT_GROUP_SYNC(void *pvParameters){
-    const TickType_t xDelay100ms = pdMS_TO_TICKS( 100UL );
+    const TickType_t xDelay100ms = pdMS_TO_TICKS( 1000UL );
     int i = 0;
     while (1) {
         // 开始 做
-        printf("%s is do  one %d \r\n", (char * ) pvParameters, i);
+        printf("%s doing %d \r\n", (char * ) pvParameters, i);
         // 我已经做好 ，要等 所有人 都做好
         xEventGroupSync(EVENT_GROUP_CALC, THREE, ALL, portMAX_DELAY);
-        printf("%s is all Done  %d  \r\n", (char * ) pvParameters, i++);
+        printf("%s Done %d  \r\n", (char * ) pvParameters, i++);
         vTaskDelay(xDelay100ms);
     }
 }
@@ -112,7 +112,7 @@ void START_TASK_EVENT_GROUP_SYNC(void *pvParameters)
             vTASK1_EVENT_GROUP_SYNC,
             "Task1",
             vTASK1_EVENT_GROUP_SYNC_FUNCTION_uxStackDepth,
-            "vTask1 params",
+            "vTask1 ",
             vTASK1_EVENT_GROUP_SYNC_FUNCTION_uxPriority,
             &TASK1_HANDLER_EVENT_GROUP_SYNC
     );
@@ -120,7 +120,7 @@ void START_TASK_EVENT_GROUP_SYNC(void *pvParameters)
             vTASK2_EVENT_GROUP_SYNC, // 任务函数
             "vTask2",// 函数 名称， 任务名称长度不要超过  configMAX_TASK_NAME_LEN。
             vTASK2_EVENT_GROUP_SYNC_FUNCTION_uxStackDepth,// 任务堆栈大小 ，注意 ： 实际申请到的堆栈是 uxStackDepth 的 4 倍（ 一个 StackType_t 是 32 位  4 个字节）， 其中空闲任务 的堆栈大小为    configMINIMAL_STACK_SIZE。
-            "vTask2 params",// 传递给任务函数的参数
+            "vTask2 ",// 传递给任务函数的参数
             vTASK2_EVENT_GROUP_SYNC_FUNCTION_uxPriority,// 任务优先级 范围 0～ configMAX_PRIORITIES-1
             &TASK2_HANDLER_EVENT_GROUP_SYNC // 任务句柄，任务创建成功以后会返回次惹怒我的任务句柄， 这个 句柄其实就是任务的 任务堆栈，此参数 就用来保存这个任务句柄；其他API函数可能会使用到这个 句柄
     );
@@ -128,11 +128,13 @@ void START_TASK_EVENT_GROUP_SYNC(void *pvParameters)
             vTASK3_EVENT_GROUP_SYNC, // 任务函数
             "vTask3",// 函数 名称， 任务名称长度不要超过  configMAX_TASK_NAME_LEN。
             vTASK3_EVENT_GROUP_SYNC_FUNCTION_uxStackDepth,// 任务堆栈大小 ，注意 ： 实际申请到的堆栈是 uxStackDepth 的 4 倍（ 一个 StackType_t 是 32 位  4 个字节）， 其中空闲任务 的堆栈大小为    configMINIMAL_STACK_SIZE。
-            "vTask3 params",// 传递给任务函数的参数
+            "vTask3 ",// 传递给任务函数的参数
             vTASK3_EVENT_GROUP_SYNC_FUNCTION_uxPriority,// 任务优先级 范围 0～ configMAX_PRIORITIES-1
             &TASK3_HANDLER_EVENT_GROUP_SYNC // 任务句柄，任务创建成功以后会返回次惹怒我的任务句柄， 这个 句柄其实就是任务的 任务堆栈，此参数 就用来保存这个任务句柄；其他API函数可能会使用到这个 句柄
     );
-    vTaskDelete(START_TASK_HANDLER_EVENT_GROUP_SYNC); //删除开始任务;  为什么执行完成要删除？？？
+
+    vTaskStartScheduler();// 开启任务调度器;
+
     //taskEXIT_CRITICAL();            //退出临界区
 }
 
@@ -141,14 +143,6 @@ void START_TASK_EVENT_GROUP_SYNC(void *pvParameters)
 void FREERTOS_EVENT_GROUP_SYNC_MAIN(){
     Serial_Init();
     RetargetInit(USART1);
-    xTaskCreate(
-            START_TASK_EVENT_GROUP_SYNC,
-            "START_TASK_EVENT_GROUP_SYNC",
-            START_STK_SIZE,
-            NULL,
-            START_TASK_EVENT_GROUP_SYNC_PRIO,
-            &START_TASK_HANDLER_EVENT_GROUP_SYNC
-    );
 
-    vTaskStartScheduler();// 开启任务调度器;
+    START_TASK_EVENT_GROUP_SYNC("pvParameters");
 }
